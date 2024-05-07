b0ca184aba93433dd8737f5f26291a5f9c4b35e1
Phantom <phantomanalytical@gmail.com>
2024-05-07 11:14:52 -0400

    Fixed power definition in sx126x.py

--- sx126x.py
+++ sx126x.py
@@ -3,6 +3,32 @@ import serial
 import time
 
 class sx126x:
+    UART_BAUDRATE = {
+        1200: 0x00,
+        2400: 0x20,
+        4800: 0x40,
+        9600: 0x60,
+        19200: 0x80,
+        38400: 0xA0,
+        57600: 0xC0,
+        115200: 0xE0
+    }
+
+    PACKET_SIZE = {
+        240: 0x00,
+        128: 0x40,
+        64: 0x80,
+        32: 0xC0
+    }
+
+    POWER_SETTING = {
+        22: 0x00,
+        17: 0x01,
+        13: 0x02,
+        10: 0x03
+    }
+
+
     def __init__(self, serial_num, freq, addr, power, rssi, air_speed=2400, net_id=0, buffer_size=240, crypt=0, relay=False, lbt=False, wor=False):
         self.M0 = 22
         self.M1 = 27
@@ -30,10 +56,24 @@ class sx126x:
 
         self.update_module_settings()
 
-    def update_module_settings(self):
-        settings = bytearray([0xC0, self.addr & 0xFF, (self.addr >> 8) & 0xFF, self.net_id, 0x60,
-                              0x00, 0x00, 0x00, 0x00])  # Assuming air_speed of 9600, packet size of 240 bytes, power of 22dBm
+    def update_module_settings(self, freq=None, addr=None, power=None, air_speed=None, buffer_size=None):
+        if freq is not None:
+            self.freq = freq
+        if addr is not None:
+            self.addr = addr
+        if power is not None:
+            self.power = power
+        if air_speed is not None:
+            self.air_speed = air_speed
+        if buffer_size is not None:
+            self.buffer_size = buffer_size
+
+        settings = bytearray([0xC0, self.addr & 0xFF, (self.addr >> 8) & 0xFF, self.net_id,
+                              self.UART_BAUDRATE[self.air_speed], self.PACKET_SIZE[self.buffer_size],
+                              self.POWER_SETTING[self.power], 0x00, 0x00])
         self.ser.write(settings)
+        print("Module settings updated.")
+
 
     def send(self, data):
         print("Sending data...")
